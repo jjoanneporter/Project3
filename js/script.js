4b7d79bdc8a70435f2b74d477bbf473dcd9dbaa8
jjoanneporter <jporter9@hawk.iit.edu>
2025-04-15 23:27:25 -0500

    map created

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let map;
 
 function initMap() {
-  const center = { lat: 41.8781, lng: -87.6298 }; // Centered near Chicago
+  const center = { lat: 41.8781, lng: -87.6298 }; // Near Chicago
 
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
@@ -15,25 +15,25 @@ function initMap() {
     {
       title: "University of Oregon",
       position: { lat: 44.0450, lng: -123.0664 },
-      info: "Eugene, Oregon",
+      info: "Eugene, Oregon<br>I have a lot of friends that go there.",
       image: "Images/uoregon-icon.png"
     },
     {
       title: "University of Utah",
       position: { lat: 40.7608, lng: -111.8910 },
-      info: "Salt Lake City, Utah",
+      info: "Salt Lake City, Utah<br>I have visited a couple times and have a lot of friends there.",
       image: "Images/uofu-icon.png"
     },
     {
       title: "Illinois Institute of Technology",
       position: { lat: 41.8349, lng: -87.6270 },
-      info: "Chicago, Illinois",
+      info: "Chicago, Illinois<br>This is where I go to school now!",
       image: "Images/iit-icon.png"
     },
     {
       title: "St. Thomas Aquinas College",
       position: { lat: 41.0614, lng: -73.9474 },
-      info: "Sparkill, New York",
+      info: "Sparkill, New York<br>I went here for my first year of college.",
       image: "Images/stac-icon.png"
     }
   ];
@@ -50,10 +50,12 @@ function initMap() {
     });
 
     const infoWindow = new google.maps.InfoWindow({
-      content: `<div style="text-align:center;">
-                  <strong>${loc.title}</strong><br>
-                  ${loc.info}
-                </div>`
+      content: `
+        <div style="text-align:center; color: #000;">
+          <strong>${loc.title}</strong><br>
+          ${loc.info}
+        </div>
+      `
     });
 
     marker.addListener("click", () => {
