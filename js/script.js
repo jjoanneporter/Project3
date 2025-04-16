1af6aa8fda60856aa3429d931bcfd335f2b9488a
jjoanneporter <jporter9@hawk.iit.edu>
2025-04-15 22:59:32 -0500

    add map html

---
+++ js/script.js
@@ -0,0 +1,63 @@
+let map;
+
+function initMap() {
+  const center = { lat: 41.8781, lng: -87.6298 }; // Centered near Chicago
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: center,
+    mapTypeControl: false,
+    streetViewControl: false,
+    zoomControl: true
+  });
+
+  const locations = [
+    {
+      title: "University of Oregon",
+      position: { lat: 44.0450, lng: -123.0664 },
+      info: "Eugene, Oregon",
+      image: "Images/uoregon-icon.png"
+    },
+    {
+      title: "University of Utah",
+      position: { lat: 40.7608, lng: -111.8910 },
+      info: "Salt Lake City, Utah",
+      image: "Images/uofu-icon.png"
+    },
+    {
+      title: "Illinois Institute of Technology",
+      position: { lat: 41.8349, lng: -87.6270 },
+      info: "Chicago, Illinois",
+      image: "Images/iit-icon.png"
+    },
+    {
+      title: "St. Thomas Aquinas College",
+      position: { lat: 41.0614, lng: -73.9474 },
+      info: "Sparkill, New York",
+      image: "Images/stac-icon.png"
+    }
+  ];
+
+  locations.forEach(loc => {
+    const marker = new google.maps.Marker({
+      position: loc.position,
+      map: map,
+      title: loc.title,
+      icon: {
+        url: loc.image,
+        scaledSize: new google.maps.Size(50, 50)
+      }
+    });
+
+    const infoWindow = new google.maps.InfoWindow({
+      content: `<div style="text-align:center;">
+                  <strong>${loc.title}</strong><br>
+                  ${loc.info}
+                </div>`
+    });
+
+    marker.addListener("click", () => {
+      infoWindow.open(map, marker);
+    });
+  });
+}
